8a3c7174d95928d6127008126385e7bad2d1efbe
ericksjp <ericksjp703@gmail.com>
2024-12-08 01:13:21 -0300

    implement estadoController functions

---
+++ src/controllers/estadoController.js
@@ -0,0 +1,28 @@
+import * as se from "../services/estadoService";
+
+export async function getAllEstados(_, res) {
+  try {
+    const estados = await se.getEstados();
+    res.send(estados);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function getEstado(req, res) {
+  try {
+    const estado = await se.getEstadoByUf(req.params.uf);
+    res.send(estado);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function getMunicipiosOfEstado(req, res) {
+  try {
+    const municipios = await se.getMunicipiosByUf(req.params.uf);
+    res.send(municipios);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
