ef7de80154a1287ac67c6481e1f7cd0686d86229
ericksjp <ericksjp703@gmail.com>
2024-12-07 23:58:42 -0300

    implement estadoService functions

---
+++ src/services/estadoService.js
@@ -0,0 +1,19 @@
+import pool from "../config/dbconfig.js";
+
+export async function getEstados() {
+  const query = "SELECT nome, sigla, regiao, area_km2, getViewbox(nome) as viewbox, st_assvg(geom) as svg FROM estado";
+  const { rows } = await pool.query(query);
+  return rows;
+}
+
+export async function getEstadoByUf(uf) {
+  const query = "SELECT nome, sigla, regiao, area_km2, getViewbox(nome) as viewbox, st_assvg(geom) as svg FROM estado where uf ilike $1";
+  const { rows } = await pool.query(query, [uf]);
+  return rows[0];
+}
+
+export async function getMunicipiosByUf(uf) {
+  const query = "SELECT nome, estado, area_km2, st_assvg(geom) as svg FROM municipio where estado ilike $1";
+  const { rows } = await pool.query(query, [uf]);
+  return rows;
+}
