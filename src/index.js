a1680b9e946c9e850bd6aecde3ae203cd161b2c9
ericksjp <ericksjp703@gmail.com>
2024-12-08 11:36:41 -0300

    add cors to api entrypoint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import estadoRoute from "./routes/estadoRoute.js";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
@@ -7,6 +8,7 @@ const __dirname = dirname(dirname(fileURLToPath(import.meta.url)));
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 
 app.use("/static", express.static(join(__dirname, "public")));
 
